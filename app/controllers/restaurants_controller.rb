396e6fbb68579d72f37c99f1c5ba4a42e6632e90
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-07-21 23:31:38 +0200

    adding create and show actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,12 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  
+  def create
+    respond_with(Restaurant.create(params[:restaurant])) # redirect to restaurant_url (show)
+  end
+  
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
